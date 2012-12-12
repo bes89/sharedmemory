3e973cbfa91a2f6c46966253434f2960a8119f78
Besnik Br <besnik.br@googlemail.com>
2012-12-12 23:39:01 +0100

    implemented ArrayAccess interface

--- SharedMemory.php
+++ SharedMemory.php
@@ -5,7 +5,7 @@
  * @author Fabian Frick <fabi.kcirf@gmail.com>
  * @author Besnik Brahimi <besnik.br@gmail.com>
  */
-class SharedMemory
+class SharedMemory implements ArrayAccess
 {
     /**
      * @var array
@@ -46,7 +46,7 @@ class SharedMemory
     {
         if (!array_key_exists($key, $this->values)) {
             if (!is_long($key)) {
-                throw new InvalidArgumentException('Expected type long for "key" but "' . gettype($key) . '" given.');
+                throw new InvalidArgumentException(sprintf('Expected type long for "key" but "%s" given.', gettype($key)));
             }
             $this->values[$key] = array(
                 'shm' => shm_attach($key, $size),
@@ -87,4 +87,60 @@ class SharedMemory
         return $value;
     }
 
+    /**
+     * Checks whether a offset exists
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset An offset to check for.
+     * @return boolean true on success or false on failure.
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->values);
+    }
+
+    /**
+     * Retrieves a variable by offset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset The offset to retrieve.
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * Sets value at the given offset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param int $offset The offset to assign the value to.
+     * @param mixed $value The value to set.
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->set($offset, $value);
+    }
+
+    /**
+     * Unsets a variable at the given offset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param int $offset The offset to unset.
+     * @throws InvalidArgumentException
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        if (!array_key_exists($offset, $this->values)) {
+            throw new InvalidArgumentException(sprintf('Undefined index "%s"'));
+        }
+
+        if (shm_remove_var($this->values[$offset]['shm'], $offset))
+        {
+            unset($this->values[$offset]);
+        }
+    }
 }
